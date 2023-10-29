a443860d08fccdb178a8378226ecfc2c065bc53e
mfghir <fatemeghafari77@gmail.com>
2023-10-29 01:02:01 -0700

    show alert

--- js/app.js
+++ js/app.js
@@ -2,7 +2,23 @@ const taskInput = document.getElementById("task-input");
 const dateInput = document.getElementById("date-input");
 const addButton = document.getElementById("add-button");
 
+const alertMessage = document.getElementById("alert-message");
+
 const todos = [];
+
+const showAlert = (message, type) => {
+  alertMessage.innerHTML = "";
+  const alert = document.createElement("p");
+  alert.innerText = message;
+
+  alert.classList.add("alert");
+  alert.classList.add(`alert-${type}`);
+  alertMessage.append(alert);
+
+  setTimeout(() => {
+    alert.style.display = "none";
+  }, 2000);
+};
 
 const addHandler = () => {
   const task = taskInput.value;
