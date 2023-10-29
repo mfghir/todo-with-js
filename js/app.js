2f87fcc77ca7b15c3c260d08a6dd71de88c3474a
mfghir <fatemeghafari77@gmail.com>
2023-10-29 00:37:47 -0700

    give todo value

---
+++ js/app.js
@@ -0,0 +1,25 @@
+const taskInput = document.getElementById("task-input");
+const dateInput = document.getElementById("date-input");
+const addButton = document.getElementById("add-button");
+
+const todos = [];
+
+const addHandler = () => {
+  const task = taskInput.value;
+  const date = dateInput.value;
+  const todo = {
+    task: task,
+    date: date,
+    completed: false,
+  };
+
+  if (task) {
+    todos.push(todo);
+    taskInput.value = "";
+    dateInput.value = "";
+  } else {
+    alert("warning");
+  }
+};
+
+addButton.addEventListener("click", addHandler);
